e12e2a357b08fa174677c8cb130a66e18504b1ba
Chris Gårdenberg <webmaster@itssimple.se>
2020-07-01 21:26:04 +0200

    refix: Skipping typescript.. it just fubar'd everything.

--- src/test.ts
+++
@@ -1,8 +0,0 @@
-import { OverwolfGameWithEventSupport, getSupportedFeatures, getSupportedFeaturesOnline } from "./index";
-
-let features = getSupportedFeatures(OverwolfGameWithEventSupport.APEX);
-console.log(features);
-
-getSupportedFeaturesOnline(OverwolfGameWithEventSupport.APEX, data => {
-  console.log(data);
-});
--- test.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const index_1 = require("./index");
-let features = index_1.getSupportedFeatures(index_1.OverwolfGameWithEventSupport.APEX);
-console.log(features);
-index_1.getSupportedFeaturesOnline(index_1.OverwolfGameWithEventSupport.APEX, data => {
-    console.log(data);
-});
